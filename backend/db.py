33437c5bf75f87c404bf0e495add2f95bd59712a
saiKelkar <kelkars1999@gmail.com>
2025-08-28 21:49:08 +0530

    Add initial backend database setup and .env ignore

---
+++ backend/db.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+
+Base = declarative_base()
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
