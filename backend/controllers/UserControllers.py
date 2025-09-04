c6eb9a8ccf6c833f5bf9063b42d0555e8aff2321
saiKelkar <kelkars1999@gmail.com>
2025-09-04 23:08:45 +0530

    Update UserControllers.py

--- backend/controllers/UserControllers.py
+++ backend/controllers/UserControllers.py
@@ -46,7 +46,7 @@ def login(request: schemas.UserLogin, db: Session):
         key="session_id", 
         value=session_id, 
         httponly=True,
-        samesite="lax"
+        samesite="none"
     )
     return response
 
