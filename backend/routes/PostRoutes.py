0159f90f4aeeb32aa4de41dc700e0b6a073ad702
saiKelkar <kelkars1999@gmail.com>
2025-09-05 14:54:18 +0530

    Improve error handling in PostRoutes and reorder endpoints

--- backend/routes/PostRoutes.py
+++ backend/routes/PostRoutes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, Form, UploadFile, File, HTTPException, R
 from sqlalchemy.orm import Session
 from pathlib import Path
 from openai import OpenAI
-import base64
+import base64, traceback
 
 import db, schemas
 from models import Posts, Users
@@ -15,10 +15,6 @@ UPLOAD_DIR = Path("uploads")
 UPLOAD_DIR.mkdir(parents=True, exist_ok=True)
 
 client = OpenAI(api_key="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")
-
-@router.get("/", response_model=list[schemas.PostResponse])
-def get_pins(db: Session=Depends(db.get_db)):
-    return PostControllers.get_pins(db)
 
 @router.post("/suggest_keywords")
 async def suggest_keywords(file: UploadFile = File(...)):
@@ -41,14 +37,13 @@ async def suggest_keywords(file: UploadFile = File(...)):
 
         return {"tags": tags}
     except Exception as e:
+        print("=== ERROR in /pin/suggest_keywords ===")
+        traceback.print_exc()
         raise HTTPException(
             status_code=500,
             detail=f"Keyword suggestion failed: {str(e)}"
         )
 
-@router.get("/{id}", response_model=schemas.PostResponse)
-def get_pin_by_id(id: int, db: Session=Depends(db.get_db)):
-    return PostControllers.get_pin_by_id(id, db)
 
 @router.post("/", response_model=schemas.PostResponse)
 async def create_pin(
@@ -89,8 +84,13 @@ async def create_pin(
             )
             try:
                 keyword_list = eval(response.choices[0].message.content)
-            except Exception:
-                keyword_list = [response.choices[0].message.content]
+            except Exception as e:
+                print("=== ERROR in /pin/suggest_keywords ===")
+                traceback.print_exc()
+                raise HTTPException(
+                    status_code=500,
+                    detail=f"Keyword suggestion failed: {str(e)}"
+                )
 
         # create db object
         pin = Posts(
@@ -108,6 +108,14 @@ async def create_pin(
         return pin
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Pin creation failed: {str(e)}")
+
+@router.get("/", response_model=list[schemas.PostResponse])
+def get_pins(db: Session=Depends(db.get_db)):
+    return PostControllers.get_pins(db)
+
+@router.get("/{id}", response_model=schemas.PostResponse)
+def get_pin_by_id(id: int, db: Session=Depends(db.get_db)):
+    return PostControllers.get_pin_by_id(id, db)
 
 @router.put("/{id}", response_model=schemas.PostResponse)
 async def update_pin(id: int, pin: schemas.PostUpdate, db: Session=Depends(db.get_db)):
